772903ffe4c913b68fb1c7902402e97dda62ce2d
Jonatas Gomes <jonatasgomes@gmail.com>
2024-05-13 12:07:19 -0400

    office

--- basic-concepts/src/App.vue
+++ basic-concepts/src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input v-model="newTask" placeholder="Add new task" />
+    <button @click="addTask">Add Task</button>
+    <ul>
+      <li v-for="task in filteredTasks" :key="task.id">
+        <input type="checkbox" v-model="task.completed" />
+        {{ task.text }}
+      </li>
+    </ul>
+    <button @click="currentFilter = 'all'">All</button>
+    <button @click="currentFilter = 'active'">Active</button>
+    <button @click="currentFilter = 'completed'">Completed</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from 'vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const newTask = ref('');
+    const tasks = ref([]);
+    const currentFilter = ref('all');
+
+    function addTask() {
+      tasks.value.push({ id: tasks.value.length, text: newTask.value, completed: false })
+      newTask.value = '';
+    }
+
+    const filteredTasks = computed(() => {
+      if (currentFilter.value === 'all') return tasks.value;
+      if (currentFilter.value === 'active') return tasks.value.filter(task => !task.completed);
+      if (currentFilter.value === 'completed') return tasks.value.filter(task => task.completed);
+      return '';
+    });
+
+    return { newTask, tasks, addTask, filteredTasks, currentFilter }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
