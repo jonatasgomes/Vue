424a0d2e7de39c91fc3f64155de83c4d3a5ec147
Jonatas Gomes Alves <jonatasgomes@gmail.com>
2024-05-12 23:00:07 -0400

    home

--- image-app/src/App.vue
+++ image-app/src/App.vue
@@ -1,47 +1,23 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import SideBar from './components/SideBar.vue';
+  import ImageGallery from './components/ImageGallery.vue';
+  import { ref } from "vue"
+
+  const isWithColor = ref(true)
+  const onSelectOption = (isColor) => {
+    isWithColor.value = isColor
+  }
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <v-app>
+    <SideBar @select-option="onSelectOption"/>
+    <v-main>
+      <ImageGallery :isWithColor="isWithColor"/>
+    </v-main>
+  </v-app>
 </template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  >>> .red-icon .v-icon {
+    color: gold;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ image-app/src/components/SideBar.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { defineEmits } from "vue"
+
+  const emits = defineEmits(["selectOption"])
+  const selectOption = (isColor) => {
+    emits("selectOption", isColor)
+  }
+</script>
+<template>
+  <v-navigation-drawer expand-on-hover rail permanent>
+    <v-list>
+      <v-list-item prepend-avatar="../../public/photo_jonatas.jpg" subtitle="jonatasgomes@gmail.com"
+        title="Jonatas Gomes"></v-list-item>
+    </v-list>
+    <v-divider></v-divider>
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-format-color-fill" title="Color Gallery" value="color" class="red-icon" @click="selectOption(true)"></v-list-item>
+      <v-list-item prepend-icon="mdi-invert-colors" title="Grayscale Gallery" value="gray" @click="selectOption(false)"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
