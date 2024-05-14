1065951bb012956d207bb0565ced5afa6998f8cd
Jonatas Gomes Alves <jonatasgomes@gmail.com>
2024-05-14 10:26:03 -0400

    home

---
+++ vuetify-key-concepts/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-card>
+        <v-row class="ma-0 pa-0">
+          <v-col cols="6" class="bg-blue-grey-lighten-2">
+            <v-img src="./assets/logo.png" :width="500" cover></v-img>
+            <h1 class="text-center">Vuetify Company</h1>
+          </v-col>
+          <v-col cols="6" class="bg-blue-grey-darken-3 d-flex flex-column justify-center align-center">
+            <div>
+              <div>
+                <h2>Jonatas Gomes</h2>
+                <p>CEO, Story Teller Company</p>
+              </div>
+              <div class="d-flex pt-4">
+                <v-icon class="mr-2">mdi-phone</v-icon>
+                <p>(X78) 228 216X</p>
+              </div>
+              <div class="d-flex pt-4">
+                <v-icon class="mr-2">mdi-map-marker</v-icon>
+                <p>Ontario, Canada</p>
+              </div>
+              <div class="d-flex pt-4">
+                <v-icon class="mr-2">mdi-email</v-icon>
+                <p>jg@gmail.com</p>            
+              </div>
+            </div>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-main>
+  </v-app>
+</template>
