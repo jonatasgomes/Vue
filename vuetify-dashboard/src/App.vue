0b2a4646f2c64d66f6ebe602970881f5cc84534f
Jonatas Gomes Alves <jonatasgomes@gmail.com>
2024-05-14 19:03:00 -0400

    home

--- vuetify-dashboard/src/App.vue
+++ vuetify-dashboard/src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-app-bar app color="primary">
+      <v-app-bar-title>Vuetify Dashboard</v-app-bar-title>
+      <v-btn rounded>Home</v-btn>
+      <v-btn rounded>Login</v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-card width="400" class="mx-auto mt-8">
+        <v-card-title>
+          <h1 class="display-1">Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+            <v-text-field :type="showPassword ? 'text' : 'password'" label="Password" prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" @click:append="showPassword = !showPassword" />
+          </v-form>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn color="info">Register</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="success">Login</v-btn>
+          </v-card-actions>
+        </v-card-text>
+      </v-card>
+    </v-main>
+    <v-footer color="primary flex-column" app>{{ new Date().getFullYear() }} - Vuetify Dashboard</v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data: () => ({
+    showPassword: false,
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
