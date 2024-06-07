4e73961222bfaa1377b1aea7ea4b74e34f6ce07e
Jonatas Gomes Alves <jonatasgomes@gmail.com>
2024-06-06 23:26:07 -0400

    home

---
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <button class="btn" @click="decreaseCounter">-</button>
+    <span class="counter">{{ count }}</span>
+    <button class="btn" @click="increaseCounter">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 10
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.count++
+    },
+    decreaseCounter() {
+      this.count--
+    }
+  }
+}
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn, .counter {
+  font-size: 40px;
+  margin: 0 10px;
+}
+</style>
